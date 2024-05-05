6169cde9d55ffa4dbe22ee59736aae76957b86a1
TRex-Dino <missionmyin@gmail.com>
2024-05-05 21:28:34 +0300

    fix lint rules

--- SwiftLint/MaybellineJsonParse/MaybellineJsonParse/ViewControllers/MaybellineCollectionViewController.swift
+++ SwiftLint/MaybellineJsonParse/MaybellineJsonParse/ViewControllers/MaybellineCollectionViewController.swift
@@ -8,48 +8,52 @@
 import UIKit
 
 class MaybellineCollectionViewController: UIViewController {
-	
-	@IBOutlet weak var collectionView: UICollectionView!
-	
-	var maybellines: [Maybelline] = []
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		fetchMaybs()
-	}
-	
-	private func fetchMaybs() {
-		NetworkMaybellineManager.shared.fetchMaybellineInfo(from: NetworkMaybellineManager.shared.urlAPI) { result in
-			switch result {
-			case .success(let maybellines):
-				DispatchQueue.main.async {
-					self.maybellines = maybellines
-					self.collectionView.reloadData()
-				}
-			case .failure(let error):
-				print(error)
-			}
-		}
-	}
+
+  @IBOutlet private weak var collectionView: UICollectionView!
+
+  var maybellines: [Maybelline] = []
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    fetchMaybs()
+  }
+
+  private func fetchMaybs() {
+    NetworkMaybellineManager.shared.fetchMaybellineInfo(from: NetworkMaybellineManager.shared.urlAPI) { result in
+      switch result {
+      case .success(let maybellines):
+        DispatchQueue.main.async {
+          self.maybellines = maybellines
+          self.collectionView.reloadData()
+        }
+      case .failure(let error):
+        print(error)
+      }
+    }
+  }
 }
 
 extension MaybellineCollectionViewController: UICollectionViewDataSource {
-	// MARK: - UICollectionViewDataSource
-	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-		maybellines.count
-	}
-	
-	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MaybellineCollectionViewCell
-		let maybs = maybellines[indexPath.item]
-		cell.configure(with: maybs)
-		return cell
-	}
-	
-	func numberOfSections(in collectionView: UICollectionView) -> Int {
-		1
-	}
-	
+  // MARK: - UICollectionViewDataSource
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    maybellines.count
+  }
+
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard
+      let cell = collectionView.dequeueReusableCell(
+        withReuseIdentifier: "cell",
+        for: indexPath
+      ) as? MaybellineCollectionViewCell
+    else {
+      return UICollectionViewCell()
+    }
+    let maybs = maybellines[indexPath.item]
+    cell.configure(with: maybs)
+    return cell
+  }
+
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    1
+  }
 }
-
-
